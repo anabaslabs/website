177a1fd25f552eb825d831564b9c2e722bbc10b5
TuhinPramanik4 <tuhinp475@gmail.com>
2025-12-31 19:03:04 +0530

    Add Hero section animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
+"use client"
+import PixelBlast from '../components/PixelBlast';
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center min-h-screen">
-      <h1 className="text-4xl sm:text-6xl font-lexend">Coming Soon...</h1>
+    <main style={{ width: '100%', height: '600px', position: 'relative' }}>
+     <PixelBlast
+
+    variant="circle"
+
+    pixelSize={12}
+
+    color="#ec4e0c"
+
+    patternScale={3}
+
+    patternDensity={1}
+
+    pixelSizeJitter={1}
+
+    // enableRipples
+
+    rippleSpeed={0.4}
+
+    rippleThickness={0.12}
+
+    rippleIntensityScale={2}
+
+    liquid
+
+    liquidStrength={0.12}
+
+    liquidRadius={0.5}
+
+    liquidWobbleSpeed={2}
+
+    speed={0.2}
+
+    edgeFade={0}
+
+    transparent
+
+  />
     </main>
   );
 }
