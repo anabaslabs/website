31af5a3f32eee2f4f3801561de6d39434b6d63dc
ankit-4623 <ankitnayek007@gmail.com>
2026-01-03 22:52:37 +0530

    contact page has been  added on main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./_components/Hero";
 import Aboutus from "./_components/Aboutus";
+import ContactPage from "./_components/Contact";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-start items-center min-h-screen">
       <Hero />
       <Aboutus />
+      <ContactPage/>
     </main>
   );
 }
