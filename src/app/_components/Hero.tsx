88b3eba2031c68dfe817468e04891df310c0c689
TuhinPramanik4 <tuhinp475@gmail.com>
2026-01-11 10:33:09 +0530

    Change text in Hero

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -53,14 +53,17 @@ export default function Hero() {
 
       <div className="relative text-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 z-10">
         <div className="flex justify-center items-center gap-3 font-lexend text-3xl md:text-5xl mb-6 text-foreground font-semibold">
-          <p>Turning real problems into scalable</p>
-          <WordRotate
-            words={["Software", "Systems", "Apps", "Agents"]}
+          <p>Empowering modern enterprises with  scalable software solutions.</p>
+        </div>
+       <div className="flex justify-center items-center gap-3 font-lexend text-2xl md:text-3xl mb-6 text-foreground font-semibold">
+          <h1>We Build</h1>
+           <WordRotate
+            words={["Software", "Systems","Websites", "Apps", "Agents"]}
             className="text-primary"
           />
-        </div>
+       </div>
         <h2 className="font-lora text-2xl md:text-3xl mb-8 text-muted-foreground font-semibold">
-          Built publicly, Shipped properly.
+          Your Vision, Built with Precision.
         </h2>
         <div className="flex flex-col sm:flex-row justify-center items-center mx-auto gap-4 sm:gap-14">
           <ShinyButton
